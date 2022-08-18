c0f91b20ff7f2470d77b98d3bab39efbab06f82f
Roman <fediuninrom5991@gmail.com>
2022-08-18 22:08:37 +0200

    Lesson 12

---
+++ src/components/Content.jsx
@@ -0,0 +1,25 @@
+const Profile = () => {
+    return <div className='profile'>
+        <div>
+            <img src='https://cdn.searchenginejournal.com/wp-content/uploads/2022/06/image-search-1600-x-840-px-62c6dc4ff1eee-sej-1280x720.png'></img>
+        </div>
+        <div>
+            avatar + description
+        </div>
+        <div>
+            My posts
+        </div>
+        <div>
+            New post
+        </div>
+        <div>
+            Post1
+        </div>
+        <div>
+            Post2
+        </div>
+    </div>
+
+};
+
+export default Profile;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://www.logodesign.net/logo/line-art-house-roof-and-buildings-4485ld.png'></img>
+    </header>
+}
+
+
+export default Header;
